f87e49e013fe7581f42f7f4aac3bacc72090e034
Divyadevaki <33373264+Divyadevaki@users.noreply.github.com>
2017-11-04 19:20:52 +0530

    Create Sample.java

---
+++ Sample.java
@@ -0,0 +1,15 @@
+class Sample{
+public static void main(String ar[]){
+Scanner sc=new Scanner(System.in);
+   System.out.println("enter the value");
+   int a=sc.nextInt();
+   if(a>=0){
+       if(a>0){
+       System.out.println("number is positive");
+   }else{
+       System.out.println("number is zero");
+   }}else{
+       System.out.println("number is negative");
+   }
+   }
+}
